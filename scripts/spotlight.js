fdc8bb5dcb66f73c03ed24542b79e13bbaf3871e
BenjaminDowdle <benjamin.dowdle99@gmail.com>
2023-09-07 17:16:31 -0600

    Syncing error.

--- scripts/spotlight.js
+++ scripts/spotlight.js
@@ -20,7 +20,6 @@ function generateSpotlights(data) {
     const count = 3
     let numbers = []
     let spotlight = document.querySelector('#spotlight')
-    let items = []
 
     while(numbers.length < count) {
         let number = Math.floor(Math.random() * (data.soaps.length))
@@ -39,7 +38,7 @@ function generateSpotlights(data) {
 
         img.setAttribute("src", data.soaps[numbers[i]].small);
         button.setAttribute("type", "button");
-        button.setAttribute("class", "add-to-cart");
+        button.setAttribute("class", "add-to-cart")
         button.innerText = "Add to Cart";
         name.innerText = data.soaps[numbers[i]].name;
         price.innerText = data.soaps[numbers[i]].price;
@@ -48,16 +47,5 @@ function generateSpotlights(data) {
         card.append(img, name, price, button);
 
         spotlight.append(card);
-
-        button.addEventListener('click', () => {
-          let item = {
-            name: data.soaps[numbers[i]].name,
-            price: data.soaps[numbers[i]].price,
-            small: data.soaps[numbers[i]].small
-          }
-          items.push(item)
-          localStorage.setItem("items", JSON.stringify(items))
-          
-        })
     }
 }
