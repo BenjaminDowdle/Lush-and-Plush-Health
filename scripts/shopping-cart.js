219d6a7badc3fadd357035ed341ba3dd70154619
BenjaminDowdle <benjamin.dowdle99@gmail.com>
2023-09-07 17:53:20 -0600

    Bug fixes

---
+++ scripts/shopping-cart.js
@@ -0,0 +1,30 @@
+let items = JSON.parse(localStorage.getItem("items"));
+
+displayOrders(items);
+
+function displayOrders(items) {
+  // -----ORDER-----
+  let order = document.querySelector(".order");
+  let receipt = document.querySelector(".receipt");
+  let priceTotal = 0.0;
+
+  items.forEach((item) => {
+    let card = document.createElement("div");
+    let img = document.createElement("img");
+    let name = document.createElement("p");
+    let price = document.createElement("em");
+
+    card.setAttribute("class", "order-card");
+    img.setAttribute("src", item.small);
+    img.setAttribute("class", "order-image");
+    name.textContent = item.name;
+    price.textContent = item.price;
+
+    card.append(img, name, price);
+
+    order.append(card);
+
+    // -----RECEIPT-----
+  });
+  console.log();
+}
